bdfbfe8fdf8b5b21806aed71f2f7373729082667
jay <jayprkshpthk@gmail.com>
2021-02-13 19:19:22 +0530

    added all the files

---
+++ index.js
@@ -0,0 +1,57 @@
+let express = require("express");
+let app = express();
+let server = require("http").createServer(app);
+let io = require("socket.io")(server);
+
+//middleares
+app.use(express.static("public"));
+
+let sockets = [];
+let array_msg = [];
+
+io.on("connection", (socket) => {
+  //saveing the user
+  sockets.push(socket);
+  console.log(
+    `New user connected [${socket.id}] Total user - ${sockets.length}`
+  );
+
+  sockets.forEach((socc) => {
+    socc.emit("new_msg", array_msg);
+  });
+
+  socket.on("msg", (data) => {
+    array_msg.push(data);
+    sockets.forEach((socc) => {
+      socc.emit("new_msg", array_msg);
+    });
+  });
+
+  sockets.forEach((soc) => {
+    soc.emit(
+      "all_user",
+      sockets
+        .map((socc) => {
+          return socc.id;
+        })
+        .filter((id) => {
+          return id !== soc.id;
+        })
+    );
+  });
+
+  socket.on("disconnect", () => {
+    //remove the socket from sockets
+    sockets = sockets.filter((soc) => {
+      return soc.id !== socket.id;
+    });
+
+    //console log
+    console.log(
+      `User disconnected [${socket.id}] Total user - ${sockets.length}`
+    );
+  });
+});
+
+let port = 8080;
+server.listen(port, () => console.log("server started "));
---
+++ public/script.js
@@ -0,0 +1,41 @@
+let all_user = document.getElementById("all_users");
+let btn = document.getElementById("btn");
+let msg = document.getElementById("msg");
+let name = document.getElementById("name");
+let chat = document.getElementById("chat");
+
+let socket = new io("http://localhost:8080/");
+
+socket.on("all_user", (data) => {
+  all_user.innerHTML = "";
+  data.forEach((soc_id) => {
+    all_user.innerHTML += `<button>${soc_id} </button> <br />`;
+  });
+});
+
+btn.onclick = () => {
+  let msg_txt = msg.value;
+  let name_txt = name.value;
+
+  if (msg_txt === "" || name_txt === "") {
+    window.alert("please write a msg");
+  } else {
+    socket.emit("msg", {
+      msg: msg_txt,
+      name: name_txt,
+    });
+  }
+};
+
+window.onload = () => {
+  socket.on("new_msg", (all_msg) => {
+    chat.innerHTML = "";
+    all_msg.forEach((msg) => {
+      chat.innerHTML += `
+            <div>
+                  <b>${msg.name}</b> : <span>${msg.msg}</span>
+            </div>
+          `;
+    });
+  });
+};
